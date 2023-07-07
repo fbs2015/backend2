5b6973f7af4f6418766ff4b53ee401e7ff77a24b
fbs2015 <f.b.s2009@hotmail.com>
2023-07-06 21:14:23 -0300

    So um ajuste de commit

--- lib/app/infra/dao/clientes/get_cliente_by_email_dao.dart
+++ lib/app/infra/dao/clientes/get_cliente_by_email_dao.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 part of infra;
 
+//apenas um comentario
+
 class GetClienteByEmailDAO implements GetClienteByEmailGateway {
   final Connection connection;
 
